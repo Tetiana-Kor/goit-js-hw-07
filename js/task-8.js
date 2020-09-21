e020267da08bc1136c2d35d24edf349672756ac6
Tetiana-Kor <63547594+Tetiana-Kor@users.noreply.github.com>
2020-09-21 10:32:32 +0300

    Додає умову 8 задачі

---
+++ js/task-8.js
@@ -0,0 +1,9 @@
+const ref = {
+  input: document.querySelector('#controls input'),
+  btnRender: document.querySelector('#controls[data-action="render"]'),
+  btnDestroy: document.querySelector('#controls[data-action="destroy"]'),
+  boxEl: document.querySelector('#boxes'),
+};
+
+// createBoxes(amount);
+// destroyBoxes();
